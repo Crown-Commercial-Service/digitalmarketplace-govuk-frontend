144362883f2cf22f4493ce5ff8a88af143c6b97e
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-10-04 13:48:37 +0100

    Style logs to improve readibilty

--- tasks/gulp/build.js
+++ tasks/gulp/build.js
@@ -1,8 +1,11 @@
 const { task, src, dest} = require('gulp')
 const debug = require('gulp-debug')
+const emoji = require('node-emoji')
+const { green } = require('chalk')
+const log = require('fancy-log')
 
 task('build:govuk-frontend', async () => {
-
+  log(`${emoji.get('clipboard')}  ${green.bold('- Copying GOV.UK Frontend to package directory')}`)
   src(['node_modules/govuk-frontend/**'])
     .pipe(debug({title: 'Copied'}))
     .pipe( dest('package/govuk-frontend'))
