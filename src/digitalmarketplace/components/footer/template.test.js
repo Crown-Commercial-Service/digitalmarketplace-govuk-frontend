9f726a26885fe6e8862c1cace0e0537a22872f31
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-10-30 12:54:58 +0000

    Add basic automated accessiblity testing to Footer component

--- src/digitalmarketplace/components/footer/template.test.js
+++ src/digitalmarketplace/components/footer/template.test.js
@@ -1,8 +1,17 @@
+const axe = require('../../../../lib/axe-helper')
+
 const { render, getExamples } = require('../../../../lib/jest-helpers.js')
 
 const examples = getExamples('footer')
 
 describe('footer', () => {
+  it('default example passes accessibility tests', async () => {
+    const $ = render('footer', examples.default)
+
+    const results = await axe($.html())
+    expect(results).toHaveNoViolations()
+  })
+
   it('renders a footer component with all our standard links', () => {
     const $ = render('footer', examples.default)
     expect($.html()).toMatchSnapshot()
