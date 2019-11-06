4a3d837df39f7e22eba5084878ab56af76367f3b
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-11-06 13:54:42 +0000

    Use 'url_for' macro to generate some dynamic url
    Previously in digitalmarketplace-frontend-toolkit the footer
    used these two url_for to generate the links for 'help' and 'a-z'.
    
    This component will now use url_for macro

--- src/digitalmarketplace/components/footer/template.test.js
+++ src/digitalmarketplace/components/footer/template.test.js
@@ -4,16 +4,26 @@ const { render, getExamples } = require('../../../../lib/jest-helpers.js')
 
 const examples = getExamples('footer')
 
+const urlForMock = jest.fn((param) => {
+  let value = ''
+  if (param === 'external.help') {
+    value = 'http://www.google.com'
+  } else if (param === 'external.suppliers_list_by_prefix') {
+    value = 'http://www.gov.uk'
+  }
+  return value
+})
+
 describe('footer', () => {
   it('default example passes accessibility tests', async () => {
-    const $ = render('footer', examples.default)
+    const $ = render('footer', examples.default, { url_for: urlForMock })
 
     const results = await axe($.html())
     expect(results).toHaveNoViolations()
   })
 
   it('renders a footer component with all our standard links', () => {
-    const $ = render('footer', examples.default)
+    const $ = render('footer', examples.default, { url_for: urlForMock })
     expect($.html()).toMatchSnapshot()
   })
 })
