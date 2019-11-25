ef883c1a7ba766c2f3f387db158206bb2b03dd08
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-11-25 16:39:00 +0000

    Adds dmHeader component
    We would have just used GOV.UK Frontend header component but we needed
    to customise it specifically for Digital Martketplace.
    
    1. Navigation links position - we needed to the style of all the applications
       to look consistant while we roll out govuk-frontend. Therefore we needed
       the navigation to be right aligned and the service name to be positioned
       next to the crown.
    
    2. Admin frontend - For Admin frontend we wanted a sutile difference in
       the header so users could see they were no longer in the main application.
       This does seem strange and breaks the rule about "Not using colour alone
       to convey important messages"
    
    Usage:
    
    1. Import the component
    2. Call the component using `{{ dmHeader({}) }}`
    3. For Admin frontend use `{{ dmHeader({"isAdmin": "true"}) }}`

---
+++ src/digitalmarketplace/components/header/template.test.js
@@ -0,0 +1,127 @@
+const axe = require('../../../../lib/axe-helper')
+
+const { render, getExamples } = require('../../../../lib/jest-helpers.js')
+
+const examples = getExamples('header')
+
+const urlForMock = jest.fn((param) => {
+  let value = ''
+  if (param === 'external.help') {
+    value = 'http://www.google.com'
+  } else if (param === 'external.render_login') {
+    value = 'http://www.logmein.now'
+  } else if (param === 'external.buyer_dashboard') {
+    value = 'http://www.buyer.dashboard'
+  } else if (param === 'external.supplier_dashboard') {
+    value = 'http://www.supplier.dashboard'
+  } else if (param === 'external.user_logout') {
+    value = 'http://www.logmeout.now'
+  }
+
+  return value
+})
+
+let mockedMethods
+
+describe('header', () => {
+  describe('when a user is not logged in', () => {
+    beforeAll(() => {
+      mockedMethods = {
+        url_for: urlForMock
+      }
+    })
+
+    it('default example passes accessibility tests', async () => {
+      const $ = render('header', examples.default, mockedMethods)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+    it('renders a header component with all our standard links', () => {
+      const $ = render('header', examples.default, mockedMethods)
+      expect($.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('buyer signed in', () => {
+    beforeAll(() => {
+      mockedMethods = {
+        url_for: urlForMock
+      }
+    })
+
+    it('default example passes accessibility tests', async () => {
+      const $ = render('header', examples['for Buyer'], mockedMethods)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+    it('renders a header component with all our standard links', () => {
+      const $ = render('header', examples['for Buyer'], mockedMethods)
+      expect($.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('supplier signed in', () => {
+    beforeAll(() => {
+      mockedMethods = {
+        url_for: urlForMock
+      }
+    })
+
+    it('default example passes accessibility tests', async () => {
+      const $ = render('header', examples['for Supplier'], mockedMethods)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+    it('renders a header component with all our standard links', () => {
+      const $ = render('header', examples['for Supplier'], mockedMethods)
+      expect($.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('admin or any other type of use is signed in', () => {
+    beforeAll(() => {
+      mockedMethods = {
+        url_for: urlForMock
+      }
+    })
+
+    it('default example passes accessibility tests', async () => {
+      const $ = render('header', examples['for users in other roles'], mockedMethods)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+    it('renders a header component with all our standard links', () => {
+      const $ = render('header', examples['for users in other roles'], mockedMethods)
+      expect($.html()).toMatchSnapshot()
+    })
+  })
+
+  describe('component being used in admin frontend', () => {
+    beforeAll(() => {
+      mockedMethods = {
+        url_for: urlForMock
+      }
+    })
+
+    it('default example passes accessibility tests', async () => {
+      const $ = render('header', examples['for Admin Frontend'], mockedMethods)
+
+      const results = await axe($.html())
+      expect(results).toHaveNoViolations()
+    })
+
+    it('renders a header component with all our standard links', () => {
+      const $ = render('header', examples['for Admin Frontend'], mockedMethods)
+      expect($.html()).toMatchSnapshot()
+    })
+
+    it('renders a header component with a custom class to style the bottom border', () => {
+      const $ = render('header', examples['for Admin Frontend'], mockedMethods)
+      expect($('header.govuk-header.app-admin-header').length).toBe(1)
+    })
+  })
+})
