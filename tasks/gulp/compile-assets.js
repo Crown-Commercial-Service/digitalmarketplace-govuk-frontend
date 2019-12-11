8fd19a1c1e85097b7f6e4f1d0f13f31f98a6a4b7
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-12-11 12:01:47 +0000

    WIP configure for puppeteer testing

--- tasks/gulp/compile-assets.js
+++ tasks/gulp/compile-assets.js
@@ -2,7 +2,7 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss')
 const autoprefixer = require('autoprefixer')
-// const rollup = require('gulp-better-rollup')
+const rollup = require('gulp-better-rollup')
 // const taskArguments = require('./task-arguments')
 // const gulpif = require('gulp-if')
 // const uglify = require('gulp-uglify')
@@ -27,22 +27,22 @@ scss.displayName = 'Compile : SCSS'
 
 // Compile js task for preview ----------
 // --------------------------------------
-const js = () => {
+const js = async (done) => {
   // for dist/ folder we only want compiled 'all.js' file
   // let srcFiles = isDist ? configPaths.src + 'all.js' : configPaths.src + '**/*.js'
-  const govukFrontendSrc = 'src/govuk-frontend/'
-  const srcFiles = govukFrontendSrc + 'all.js'
+  const dmFrontendSrc = 'src/digitalmarketplace/'
+  const srcFiles = dmFrontendSrc + 'all.js'
 
-  return gulp.src([
+  await gulp.src([
     srcFiles,
-    '!' + govukFrontendSrc + '**/*.test.js'
+    '!' + dmFrontendSrc + '**/*.test.js'
   ])
-    // .pipe(rollup({
-    //   // Used to set the `window` global and UMD/AMD export name.
-    //   name: 'GOVUKFrontend',
-    //   // UMD allows the published bundle to work in CommonJS and in the browser.
-    //   format: 'umd'
-    // }))
+    .pipe(rollup({
+      // Used to set the `window` global and UMD/AMD export name.
+      name: 'DMGOVUKFrontend',
+      // UMD allows the published bundle to work in CommonJS and in the browser.
+      format: 'umd'
+    }))
     // .pipe(uglify({ie8: true }))
     // .pipe(gulpif(isDist,
     //   rename({
@@ -52,6 +52,8 @@ const js = () => {
     // ))
     // .pipe(eol())
     .pipe(gulp.dest('app/public/assets/javascript/'))
+
+  await done()
 }
 
 js.displayName = 'Compile : JavaScript'
