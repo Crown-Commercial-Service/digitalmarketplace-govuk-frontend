b01add4d068e01665451971aa28ef8c5fdc1db2c
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-12-17 16:27:29 +0000

    Merges pre-release step into release step
    We feel that being a two step process is a bit overkill for what actually
    needs to be done. So we have merged the two steps together

--- bin/release.sh
+++ bin/release.sh
@@ -8,34 +8,13 @@ if [[ "$1" == "--dry-run" ]]; then
   NPM_ARGS=" --dry-run"
   echo "Starting a dry run release..."
 else
-  DRY_RUN=false
-  echo "Starting a pre-release..."
+  echo "Starting a release..."
 fi
 echo " "
 echo "This will:"
 echo "- run the test suite"
 echo "- build GOV.UK Frontend into the 'package/' directory"
 echo "- build Digital Marketplace Frontend into the 'package/' directory"
-echo " "
-
-read -r -p "Do you want to continue? [y/N] " continue_prompt
-
-if [[ $continue_prompt != 'y' ]]; then
-    echo "Cancelling pre-release, if this was a mistake, try again and use 'y' to continue."
-    exit 0
-fi
-
-npm run test
-npm run build
-
-
-if [[ "$1" == "--dry-run" ]]; then
-  echo "Starting a dry run release..."
-else
-  echo "Starting a release..."
-fi
-echo " "
-echo "This will:"
 echo "- check that you're logged in to npm as the correct user"
 echo "- publish the package to npmjs.org if it has not been published already"
 echo " "
@@ -46,6 +25,9 @@ if [[ $continue_prompt != 'y' ]]; then
     echo "Cancelling release, if this was a mistake, try again and use 'y' to continue."
     exit 0
 fi
+
+npm run test
+npm run build
 
 if [ "$DRY_RUN" = "false" ]; then
   NPM_USER=$(npm whoami)
