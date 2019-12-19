2b4f9022fdf0404c2fff4b74ecd40533cde7c58e
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-12-19 15:11:06 +0000

    Compile assets before testing

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
-const { series } = require('gulp')
+const { series, parallel } = require('gulp')
 const clean = require('./tasks/gulp/clean')
 const copy = require('./tasks/gulp/copy')
+const compile = require('./tasks/gulp/compile-assets')
 
 exports.postInstall = series(clean.all, copy.development)
 
 exports.build = series(clean.pkg, copy.forPublishing)
+
+exports.compiling = series(parallel(compile.js, compile.scss))
 
 exports.default = exports.postInstall
