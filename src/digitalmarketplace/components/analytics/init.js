ce104a25093bf82fd35403f5a562fbe6d63f7619
Kat Stevens <katherine.stevens@digital.cabinet-office.gov.uk>
2020-01-31 17:45:29 +0000

    Analytics component and tests
    Heavily based on GOV.UK and Notify equivalents

---
+++ src/digitalmarketplace/components/analytics/init.js
@@ -0,0 +1,38 @@
+import * as PageAnalytics from './analytics'
+
+window.DMGOVUKFrontend = window.DMGOVUKFrontend || {}
+
+// TODO: Remove hard coded tracking ID to make this more generic and useful to others
+const trackingId = 'UA-26179049-1'
+
+window[`ga-disable-${trackingId}`] = true
+
+function InitialiseAnalytics () {
+  // guard against being called more than once
+  if (!('analytics' in window.DMGOVUKFrontend)) {
+    window[`ga-disable-${trackingId}`] = false
+
+    // TODO: Check if we still need this hack for the domain
+    const cookieDomain = (document.domain === 'www.digitalmarketplace.service.gov.uk') ? '.digitalmarketplace.service.gov.uk' : document.domain
+
+    // Load Analytics libraries
+    PageAnalytics.LoadGoogleAnalytics()
+
+    // Configure profiles and make interface public
+    // for custom dimensions, virtual pageviews and events
+    window.DMGOVUKFrontend.analytics = new PageAnalytics.SetupAnalytics({
+      trackingId: trackingId,
+      cookieDomain: cookieDomain,
+      anonymizeIp: true,
+      displayFeaturesTask: null,
+      transport: 'beacon',
+      name: 'DMGOVUKFrontend.analytics',
+      expires: 365
+    })
+
+    // Track initial pageview
+    PageAnalytics.TrackPageview()
+  }
+}
+
+export default InitialiseAnalytics
