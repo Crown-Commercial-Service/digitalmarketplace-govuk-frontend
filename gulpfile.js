00f2a0e596743a0539b59e6172cc5e11be8f3673
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2020-05-28 13:32:44 +0100

    Rename gulp task compiling to compile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 const { series, parallel } = require('gulp')
 const clean = require('./tasks/gulp/clean')
 const copy = require('./tasks/gulp/copy')
-const compile = require('./tasks/gulp/compile-assets')
+const compileAssets = require('./tasks/gulp/compile-assets')
 
 const postInstall = series(clean.all, copy.development)
 
-const compile = series(parallel(compile.js, compile.scss))
+const compile = series(parallel(compileAssets.js, compileAssets.scss))
 
 const build = series(
   clean.pkg,
